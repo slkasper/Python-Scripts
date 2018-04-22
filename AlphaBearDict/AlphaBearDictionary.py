e42d03c1901f3efcf396416fb9f090c01987f285
Sonya <slkasper@gmail.com>
2018-04-22 11:47:44 -0400

    words printed to screen no longer has dups

--- AlphaBearDict/AlphaBearDictionary.py
+++ AlphaBearDict/AlphaBearDictionary.py
@@ -6,6 +6,7 @@ Created on Sat Apr  7 17:49:19 2018
 """
 import json
 from itertools import permutations
+#from  more_itertools import unique_everseen
 
 filename = "dictionary-master\dictionary.json"
 
@@ -13,8 +14,8 @@ def ask():
     '''docstring: explain what i do'''
     # then we need to output the words with the definitions.
     output_defs = str(input('What do you want to define? (press q to quit) >>> '))
-    letters = output_defs.upper()
-    return letters
+    word_to_define = output_defs.upper()
+    return word_to_define
 
 # Fist we need to download a dictionary.
 with open(filename) as json_data:
@@ -34,7 +35,7 @@ if letters.isalpha():
             if check_is_word in websters:
                 all_words.append(check_is_word)
     # print the words that are in websters
-    for word in sorted(all_words):
+    for word in sorted(set(all_words)):
         print(word)
 
     # ask if the user wants to look up a word in websters
